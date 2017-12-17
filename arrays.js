b320f9a13c83671aea03350db6de866bb421a336
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-17 22:53:53 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = [
+  'snickers', 
+  'hundred grand', 
+  'kitkat',
+  'skittles'
+  ];
+  
+  function addElementToBeginningOfArray(arr, el) {
+    return [el, ...arr];
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(arr, el) {
+    return arr.unshift(el);
+  }
+  
+  function addElementToEndOfArray(arr, el) {
+    return [...arr, el];
+  }
+  
+  function destructivelyAddElementToEndOfArray(arr, el) {
+    return arr.push(el);
+  }
+  
+  function accessElementInArray(arr, index) {
+    return arr[index];
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(arr) {
+    arr.shift();
+    return arr;
+  }
+  
+  function removeElementFromBeginningOfArray(arr) {
+    return arr.slice(1);
+  }
